ac1453fd1c7a4915064b39d253439ad7b59ac429
MariuszAKS <mariusz-k-sikora@o2.pl>
2023-07-24 19:11:27 +0200

    Added sprites and sprRend for mood
    Added player, npc, voice range and mood sprites and added effect after hearing spoilers.

--- Assets/Scripts/NpcController.cs
+++ Assets/Scripts/NpcController.cs
@@ -10,22 +10,33 @@ public class NpcController : MonoBehaviour
 
     [SerializeField] AIDestinationSetter destination;
 
+    [SerializeField] SpriteRenderer MoodSprRend;
+    [SerializeField] Sprite MoodAnnoyed;
+    [SerializeField] Sprite MoodGivenUp;
+    [SerializeField] Sprite MoodAggressive;
+
 
 
     void Awake()
     {
         destination = GetComponent<AIDestinationSetter>();
+        MoodSprRend.enabled = false;
     }
 
     public void HeardSpoiler() {
         spoilersBeforeGettingUp--;
+        MoodSprRend.enabled = true;
 
         if (spoilersBeforeGettingUp <= 0) {
             if (isAggressive) {
+                MoodSprRend.sprite = MoodAggressive;
                 destination.target = GameObject.FindWithTag("Player").transform;
             } else {
+                MoodSprRend.sprite = MoodGivenUp;
                 destination.target = GameObject.FindWithTag("Exit").transform;
             }
+        } else {
+            MoodSprRend.sprite = MoodAnnoyed;
         }
     }
 }
